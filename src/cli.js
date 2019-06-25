13d43f00771a3a63e7a2c5d10ce7c6ceffc4a8a8
Thomas Piccirello <Piccirello@users.noreply.github.com>
2019-06-25 13:33:05 -0700

    Remove run function

--- src/cli.js
+++ src/cli.js
@@ -14,35 +14,6 @@ User configurable:
 - include files/directories
  */
 
-async function run() {
-  program
-    .version(package.version)
-    .option("-p, --path <path>", "Scan the specified path")
-    .option("-r, --repo <url>", "Scan the specied git repo url")
-    .option("-b, --branch <name>", "Scan the specied git branch")
-    .option("--max-file-size <MiB>", "Maximum size of files to scan")
-    .option("--include-file-exts <list>", "File extensions to include")
-    .option("--exclude-file-exts <list>", "File extensions to exclude (e.g. \"json, map, csv\")")
-    .parse(process.argv);
-
-  let { path } = program;
-  const { repo, branch } = program;
-
-  if (repo) {
-    const tempPath = await Filesystem.makeTempDirectory();
-    await Git.clone(repo, tempPath, branch);
-    path = tempPath;
-  }
-
-  if (!path) {
-    return Promise.reject("You must specify a path or repo");
-  }
-
-  const config = getConfig(program);
-  const radar = new Radar(config);
-  radar.scan(path)
-    .then(result => console.dir(result, { depth: 3 } ));
-}
 
 function getConfig(program) {
   const { maxFileSize, includeFileExts, excludeFileExts } = program;
@@ -63,5 +34,34 @@ function getConfig(program) {
   return config;
 }
 
-run()
-  .catch((err) => console.error(err));
+return Promise.resolve()
+  .then(async () => {
+    program
+      .version(package.version)
+      .option("-p, --path <path>", "Scan the specified path")
+      .option("-r, --repo <url>", "Scan the specied git repo url")
+      .option("-b, --branch <name>", "Scan the specied git branch")
+      .option("--max-file-size <MiB>", "Maximum size of files to scan")
+      .option("--include-file-exts <list>", "File extensions to include")
+      .option("--exclude-file-exts <list>", "File extensions to exclude (e.g. \"json, map, csv\")")
+      .parse(process.argv);
+
+    let { path } = program;
+    const { repo, branch } = program;
+
+    if (repo) {
+      const tempPath = await Filesystem.makeTempDirectory();
+      await Git.clone(repo, tempPath, branch);
+      path = tempPath;
+    }
+
+    if (!path) {
+      return Promise.reject("You must specify a path or repo");
+    }
+
+    const config = getConfig(program);
+    const radar = new Radar(config);
+    return radar.scan(path);
+  })
+    .then(result => console.dir(result, { depth: 3 } ))
+    .catch((err) => console.error(err));
