16c4747a75ae10333aeff92c04486dc180747a50
Thomas Piccirello <Piccirello@users.noreply.github.com>
2019-06-28 17:14:24 -0700

    Test one additional logic branch

--- test/filters/excludepatterns.test.js
+++ test/filters/excludepatterns.test.js
@@ -1,6 +1,7 @@
 const { checkMatch } = require('../../src/filters/excludepatterns');
 
 test('Equals signs', () => {
+  expect(checkMatch("test")).toStrictEqual(1);
   expect(checkMatch("test=")).toStrictEqual(1);
   expect(checkMatch("test==")).toStrictEqual(1);
   expect(checkMatch("test===")).toStrictEqual(0);
