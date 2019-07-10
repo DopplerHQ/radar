5088f1e4061bed0249dea3fd54be74e7d85bd054
Thomas Piccirello <Piccirello@users.noreply.github.com>
2019-07-09 17:21:56 -0700

    Improve robustness of auth url detection

--- src/filters/authurl.js
+++ src/filters/authurl.js
@@ -5,7 +5,25 @@ const weight = FilterWeights.MAX;
 const negativeWeight = FilterWeights.NONE;
 
 function checkMatch(term) {
-  return (term.includes('://') && term.includes('@')) ? 1 : 0;
+  if (!term.includes('://') && !term.startsWith('//')) {
+    return 0;
+  }
+
+  const protocolIndex = term.indexOf('//');
+  const authSeparatorIndex = term.indexOf(':', protocolIndex);
+  const authPostfixIndex = term.indexOf('@');
+  // an auth url will contain a ':' to separate the username and password, followed by a '@' to separate the auth creds and the domain
+  if ((authSeparatorIndex === -1) || (authPostfixIndex === -1) || (authPostfixIndex < authSeparatorIndex)) {
+    return 0;
+  }
+
+  const domainPathSeparatorIndex = term.indexOf('/', protocolIndex + 2);
+  // third '/' (if present) should separate domain and path, and thus come after the auth characters (':' and '@')
+  if ((domainPathSeparatorIndex >= 0) && ((domainPathSeparatorIndex < authSeparatorIndex) || (domainPathSeparatorIndex < authPostfixIndex))) {
+    return 0;
+  }
+
+  return 1;
 }
 
 module.exports = { name, weight, negativeWeight, checkMatch };
--- test/filters/excludenonauthurl.test.js
+++ test/filters/excludenonauthurl.test.js
@@ -1,11 +1,14 @@
-const { checkMatch } = require('../../src/filters/excludeurl');
+const { checkMatch } = require('../../src/filters/excludenonauthurl');
 
 test('Is URL', () => {
   expect(checkMatch("https://example.com")).toStrictEqual(0);
   expect(checkMatch("https://subdomain.example.com/")).toStrictEqual(0);
   expect(checkMatch("https://example.com/test/index.html")).toStrictEqual(0);
+  expect(checkMatch("https://user@example.com")).toStrictEqual(0);
+  expect(checkMatch("//user@example.com")).toStrictEqual(0);
 });
 
 test('Is not URL', () => {
   expect(checkMatch("https://user:pass@example.com")).toStrictEqual(1);
+  expect(checkMatch("//user:pass@example.com")).toStrictEqual(1);
 });
