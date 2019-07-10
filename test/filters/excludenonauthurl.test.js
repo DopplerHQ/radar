a34890f505ba8011ad63f798cce46da4ec0fa73a
Thomas Piccirello <Piccirello@users.noreply.github.com>
2019-07-09 17:36:22 -0700

    Improve test clarity

--- test/filters/excludenonauthurl.test.js
+++ test/filters/excludenonauthurl.test.js
@@ -1,6 +1,6 @@
 const { checkMatch } = require('../../src/filters/excludenonauthurl');
 
-test('Is URL', () => {
+test('is url w/o auth', () => {
   expect(checkMatch("https://example.com")).toStrictEqual(0);
   expect(checkMatch("https://subdomain.example.com/")).toStrictEqual(0);
   expect(checkMatch("https://example.com/test/index.html")).toStrictEqual(0);
@@ -8,7 +8,11 @@ test('Is URL', () => {
   expect(checkMatch("//user@example.com")).toStrictEqual(0);
 });
 
-test('Is not URL', () => {
+test('is url w/ auth', () => {
   expect(checkMatch("https://user:pass@example.com")).toStrictEqual(1);
   expect(checkMatch("//user:pass@example.com")).toStrictEqual(1);
+});
+
+test('is not url', () => {
+  expect(checkMatch("testtesttest")).toStrictEqual(1);
 });
