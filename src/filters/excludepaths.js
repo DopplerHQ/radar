60248f4cd42e0240e1cb0d9ddd1231f571902652
Thomas Piccirello <thomas@doppler.com>
2019-07-13 19:48:07 -0700

    Filter out windows paths

--- src/filters/excludepaths.js
+++ src/filters/excludepaths.js
@@ -5,7 +5,7 @@ const weight = FilterWeights.NONE;
 const negativeWeight = FilterWeights.MAX;
 
 function checkMatch(term) {
-  const hasPathNavigation = (term.includes('../') || term.includes('./'));
+  const hasPathNavigation = (term.includes('../') || term.includes('./') || term.includes('C:\\\\') || term.includes('c:\\\\'));
   const hasMultiplePathSeparators = (!term.includes('://') && ((term.match(/\//g) || []).length >= 2));
   // we want to exclude paths, so treat a match as a negative case
   return (hasPathNavigation || hasMultiplePathSeparators) ? 0 : 1;
