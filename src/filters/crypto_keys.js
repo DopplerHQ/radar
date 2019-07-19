a1dbd8aa537ccb2b606790d8df35ffbadedc42aa
Thomas Piccirello <thomas@doppler.com>
2019-07-18 23:49:07 -0700

    Rename class

--- src/filters/crypto_keys.js
+++ src/filters/crypto_keys.js
@@ -4,11 +4,11 @@ const name = 'Identify cryptographic keys';
 
 const PrivateKeyRegex = (/^.*(BEGIN ).*(PRIVATE KEY).*$/i);
 
-class CustomFilter extends Filter {
+class CryptoKeys extends Filter {
   checkMatch(term) {
     return PrivateKeyRegex.test(term);
   }
 }
 
-const filter = new CustomFilter(name);
+const filter = new CryptoKeys(name);
 module.exports = filter;
