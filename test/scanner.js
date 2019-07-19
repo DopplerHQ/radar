f3a8431e15f57af00efac28c02d8794d1bccb3e6
Thomas Piccirello <thomas@doppler.com>
2019-07-19 00:59:37 -0700

    Add Scanner tests

---
+++ test/scanner.js
@@ -0,0 +1,95 @@
+const Scanner = require("../src/Scanner");
+const FilterWeights = require("../src/objects/filterweights");
+
+test('confidence calculation- positive weights', () => {
+  const scores = [
+    {
+      name: "filter1",
+      weight: FilterWeights.HIGH,
+      score: .9,
+    }
+  ];
+  let confidence = (FilterWeights.HIGH * .9) / FilterWeights.HIGH;
+  expect(Scanner.calculateConfidence(scores)).toStrictEqual(confidence);
+
+  scores.push({
+    name: "filter2",
+    weight: FilterWeights.MEDIUM,
+    score: .75,
+  });
+  confidence = ((FilterWeights.HIGH * .9) + (FilterWeights.MEDIUM * .75)) / (FilterWeights.HIGH + FilterWeights.MEDIUM);
+  expect(Scanner.calculateConfidence(scores)).toStrictEqual(confidence);
+
+  scores.push({
+    name: "filter3",
+    weight: FilterWeights.LOW,
+    score: .5,
+  });
+  confidence = ((FilterWeights.HIGH * .9) + (FilterWeights.MEDIUM * .75) + (FilterWeights.LOW * .5)) / (FilterWeights.HIGH + FilterWeights.MEDIUM + FilterWeights.LOW);
+  expect(Scanner.calculateConfidence(scores)).toStrictEqual(confidence);
+});
+
+test('confidence calculation- negative weights', () => {
+  const scores = [
+    {
+      name: "filter1",
+      weight: FilterWeights.HIGH,
+      score: 0,
+    }
+  ];
+  expect(Scanner.calculateConfidence(scores)).toStrictEqual(0);
+
+  scores.push({
+    name: "filter2",
+    weight: FilterWeights.MEDIUM,
+    score: 0,
+  });
+  expect(Scanner.calculateConfidence(scores)).toStrictEqual(0);
+});
+
+test('confidence calculation- mixed weights', () => {
+  const scores = [
+    {
+      name: "filter1",
+      weight: FilterWeights.HIGH,
+      score: .9,
+    },
+    {
+      name: "filter2",
+      weight: FilterWeights.MEDIUM,
+      score: 0,
+    },
+    {
+      name: "filter3",
+      weight: FilterWeights.LOW,
+      score: .5,
+    },
+  ];
+
+  const confidence = ((FilterWeights.HIGH * .9) + (FilterWeights.MEDIUM * 0) + (FilterWeights.LOW * .5)) / (FilterWeights.HIGH + FilterWeights.MEDIUM + FilterWeights.LOW);
+  expect(Scanner.calculateConfidence(scores)).toStrictEqual(confidence);
+});
+
+test('score term', () => {
+  const filters = [
+    {
+      name: "test",
+      checkMatch: () => ({ score: 1, weight: 1 }),
+    },
+    {
+      name: "another test",
+      checkMatch: () => ({ score: 0, weight: 4 }),
+    },
+  ];
+
+  expect(Scanner.scoreTerm("", filters)).toStrictEqual([
+    {
+      weight: 1,
+      score: 1,
+    },
+    {
+      weight: 4,
+      score: 0,
+    },
+  ]);
+});
