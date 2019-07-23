30406e334ba0ab5016341b707eefeca6de47c41c
Thomas Piccirello <thomas@piccirello.com>
2019-07-23 01:17:47 -0700

    Move variables into constructor

--- src/secrets/crypto_keys.js
+++ src/secrets/crypto_keys.js
@@ -2,14 +2,14 @@ const Secret = require('../Secret');
 const CryptoKeyExtentions = require('../crypto_key_extensions');
 const FileTags = require('../objects/file_tags');
 
-const name = 'crypto_key';
-const filters = ['crypto_keys'];
-// include files w/ no extension so we can support typical openssh key names (e.g. id_ed25519)
-const noExtension = '';
-const extensions = [noExtension, ...CryptoKeyExtentions.private_keys];
-
 class CryptoKeys extends Secret {
   constructor() {
+    const name = 'crypto_key';
+    const filters = ['crypto_keys'];
+    // include files w/ no extension so we can support typical openssh key names (e.g. id_ed25519)
+    const noExtension = '';
+    const extensions = [noExtension, ...CryptoKeyExtentions.private_keys];
+
     super(name, { filters, extensions });
   }
 
