c38f6b08b9c98e69f685c9e48d741b97cc14a054
Thomas Piccirello <thomas@doppler.com>
2019-07-24 12:18:10 -0700

    Support ipv6 addresses with fewer than 4 characters per group

--- test/filters/ipaddress.test.js
+++ test/filters/ipaddress.test.js
@@ -6,6 +6,8 @@ test('is ipv6 address', () => {
   expect(Filter.isMatch(`2001:0Db8:85a3:0042:1000:8a2E:0370:7334`)).toBe(true);
   expect(Filter.isMatch(`2001:0db8:85a3:0042:1000:8a2e:0370:7334/128`)).toBe(true);
   expect(Filter.isMatch(`[2001:0db8:85a3:0042:1000:8A2E:0370:7334]`)).toBe(true);
+  expect(Filter.isMatch(`b127:84ec:7726:3b3c:16d:6ac0:4f78:486b/53`)).toBe(true);
+  expect(Filter.isMatch(`b127:84:7:3b3c:16d:6a:4f78:486b/53`)).toBe(true);
 });
 
 test('is not ipv6 ip', () => {
