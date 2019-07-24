dcfe8ddca78438771e5fe196e339bbe6ce261657
Thomas Piccirello <thomas@doppler.com>
2019-07-23 17:36:44 -0700

    Support additional characters

--- test/filters/hash.test.js
+++ test/filters/hash.test.js
@@ -3,9 +3,13 @@ const Filter = require('../../src/filters/hash');
 test('hash', () => {
   expect(Filter.isMatch('md5-YICcOcv/VTNyJv1eC1IPNB8ftcY=')).toBe(true);
   expect(Filter.isMatch('sha1-YICcOcv/VTNyJv1eC1IPNB8ftcY=')).toBe(true);
+  expect(Filter.isMatch('sha2-YICcOcv/VTNyJv1eC1IPNB8ftcY=')).toBe(true);
   expect(Filter.isMatch('SHA256:vCNX7eUkdvqqW0m4PoxQAZRv+CM4P4fS8+CbliAvS4k')).toBe(true);
+  expect(Filter.isMatch('sha3-YICcOcv/VTNyJv1eC1IPNB8ftcY=')).toBe(true);
   expect(Filter.isMatch('sha512-OfC2uemaknXr87bdLUkWog7nYuliM9Ij5HUcajsVcMCpQrcLmtxRbVFTIqmcSkSeYRBFBRxs2FiUqFJDLdiebA==')).toBe(true);
   expect(Filter.isMatch('sha256-OfC2uemaknXr87bdLUkWog7nYuliM9Ij5HUcajsVcMCpQrcLmtxRbVFTIqmcSkSeYRBFBRxs2FiUqFJDLdiebA==')).toBe(true);
+  expect(Filter.isMatch('sha1-+PwEyjoTeErenhZBr5hXjPvWR6k=')).toBe(true);
+  expect(Filter.isMatch('sha1-/P+wEyjoTeErenhZBr5hXjPvWR6k=')).toBe(true);
 });
 
 test('not hash', () => {
