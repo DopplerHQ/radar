cc2af31309eecdac03743bd612a4d4893c48baa6
Thomas Piccirello <thomas@doppler.com>
2019-07-25 19:18:54 -0700

    Add filter for common false positive patterns

---
+++ src/filters/common_patterns.js
@@ -0,0 +1,19 @@
+const Filter = require('../objects/Filter');
+
+class CommonPatterns extends Filter {
+  constructor() {
+    super('Common programming patterns');
+
+    // find chained variables (e.g. foo.bar.method)
+    this.chainedVariables = /^([a-zA-Z0-9]+\.){2,}[a-zA-Z0-9]+$/;
+    // TODO write unit tests (--disable=PLUGIN1,PLUGIN2)
+    this.featureFlag = /^--[a-zA-Z0-9]+=('|")?[a-zA-Z0-9 ,]+('|")?$/;
+  }
+
+  isMatch(term) {
+    return this.chainedVariables.test(term) || this.featureFlag.test(term);
+  }
+}
+
+const filter = new CommonPatterns();
+module.exports = filter;
