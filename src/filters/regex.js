7a45f9dc05d75aa807fb9915177353028015e38b
Thomas Piccirello <thomas@doppler.com>
2019-07-26 12:09:45 -0700

    Add support for regex exclusions

--- src/filters/regex.js
+++ src/filters/regex.js
@@ -4,12 +4,12 @@ class Regex extends Filter {
   constructor() {
     super('Regex');
 
-    // match open block `[` followed by two letter patterns (full alphabet or hex). case-insensitive. examples: `[a-zA-Z`, `[A-Za-z`, `[a-fA-F`, `[A-Fa-f`, `a-za-f`, `a-fa-z`, etc
-    this.regexLettersWithLeadingBlock = /\[(a-z|a-f){2}/i;
+    // match open block `[` and optional exclusion `^`, followed by two letter patterns (full alphabet or hex). case-insensitive. examples: `[a-zA-Z`, `[A-Za-z`, `[a-fA-F`, `[A-Fa-f`, `a-za-f`, `a-fa-z`, etc
+    this.regexLettersWithLeadingBlock = /\[\^?(a-z|a-f){2}/i;
     // same as previous but with close block `]` instead of open block
     this.regexLettersWithTrailingBlock = /(a-z|a-f){2}\]/i;
-    // match open block `[` followed by letter pattern (full alphabet or hex) AND numeric pattern. case-insensitive. examples: `a-z0-9`, `a-f0-9`, `0-9a-z`, `0-9a-f`, etc
-    this.regexMixedCharsWithLeadingBlock = /\[((a-z|a-f)0-9|0-9(a-z|a-f))/i;
+    // match open block `[` and optional exclusion `^`, followed by letter pattern (full alphabet or hex) AND numeric pattern. case-insensitive. examples: `a-z0-9`, `a-f0-9`, `0-9a-z`, `0-9a-f`, etc
+    this.regexMixedCharsWithLeadingBlock = /\[\^?((a-z|a-f)0-9|0-9(a-z|a-f))/i;
     // same as previous but with close block `]` instead of open block
     this.regexMixedCharsWithTrailingBlock = /((a-z|a-f)0-9|0-9(a-z|a-f))\]/i;
     // match number of repetitions. examples: {2}, {2,}, {2,4}
