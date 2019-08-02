31afe3e6030506cbdde575678d12a7e46e38ef36
Thomas Piccirello <thomas@doppler.com>
2019-08-02 01:07:57 -0700

    Change groups to non-capture groups

--- src/filters/common_patterns.js
+++ src/filters/common_patterns.js
@@ -5,11 +5,11 @@ class CommonPatterns extends Filter {
     super('Common programming patterns');
 
     // find chained variables (e.g. foo.bar.method)
-    this.chainedVariables = /^([a-zA-Z0-9]+\.){2,}[a-zA-Z0-9]+$/;
+    this.chainedVariables = /^(?:[a-zA-Z0-9]+\.){2,}[a-zA-Z0-9]+$/;
     // TODO write unit tests (--disable=PLUGIN1,PLUGIN2)
-    this.featureFlag = /^--[a-zA-Z0-9]+=('|")?[a-zA-Z0-9 ,]+('|")?$/;
+    this.featureFlag = /^--[a-zA-Z0-9]+=(?:'|")?[a-zA-Z0-9 ,]+(?:'|")?$/;
     // find variables (${})
-    this.variableCurlyBraces = /\${.*}/;
+    this.variableCurlyBraces = /(?:\$|#){.*}/;
   }
 
   isMatch(term) {
