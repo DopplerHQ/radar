23e5a65b1fc283069604cee71513714b482ed0a1
Thomas Piccirello <thomas@doppler.com>
2019-08-05 12:40:38 -0700

    Add de domain

--- src/filters/email.js
+++ src/filters/email.js
@@ -6,7 +6,7 @@ class EmailFilter extends Filter {
     // match in this order: alphanumerics, underscores, periods, and hyphens (the user); @ sign; alphanumerics, underscores, periods, and hyphens (the domain); period; common TLDs
     // match must start at beginning of line or not be preceded by alphanumeric, underscore, hyphen, slash (/), or colon (:)
     // match must end at end of line or not be followed by alphanumeric, underscore, hyphen, slash (/), or colon (:)
-    this.emailRegex = /(?:^|[^\w-\/:])(\w(?:(\.|-)?\w)+)@(\w(?:(\.|-)?\w)+)\.(com|org|net|gov|edu|uk|ru)(?:$|[^\w-\/:])/;
+    this.emailRegex = /(?:^|[^\w-\/:])(\w(?:(\.|-)?\w)+)@(\w(?:(\.|-)?\w)+)\.(com|org|net|gov|edu|uk|ru|de)(?:$|[^\w-\/:])/;
   }
 
   isMatch(term) {
