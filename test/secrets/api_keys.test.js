e19f5cdb128d8a4619a76fc83b0faf1eda6ca6da
Thomas Piccirello <thomas@doppler.com>
2019-08-19 01:33:16 -0700

    Replace Array.reduce function with Array.some Rename functions for clarity. Moar unit tests!

--- test/secrets/api_keys.test.js
+++ test/secrets/api_keys.test.js
@@ -11,3 +11,25 @@ test('should scan', () => {
   expect(APIKeys.shouldScan(new Set([FileTags.NO_EXTENSION]))).toBe(false);
   expect(APIKeys.shouldScan(new Set([FileTags.ENV_FILE]))).toBe(false);
 });
+
+test('get terms', () => {
+  expect(APIKeys.getTerms("abc123def456ghi789jklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")).toStrictEqual(["abc123def456ghi789jklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"]);
+
+  expect(APIKeys.getTerms("function assert return")).toStrictEqual([]);
+
+  const prevMaxLineLength = APIKeys.maxLineLength;
+  APIKeys.maxLineLength = 8;
+  expect(APIKeys.getTerms("123456789")).toStrictEqual([]);
+  APIKeys.maxLineLength = prevMaxLineLength;
+});
+
+test('is alpha numeric', () => {
+  expect(APIKeys.isAlphaNumeric("abc 1 2 3")).toStrictEqual(true);
+
+  expect(APIKeys.isAlphaNumeric("abc")).toStrictEqual(false);
+  expect(APIKeys.isAlphaNumeric("abc123")).toStrictEqual(false);
+  expect(APIKeys.isAlphaNumeric("123")).toStrictEqual(false);
+  expect(APIKeys.isAlphaNumeric("1 2 3")).toStrictEqual(false);
+  // must have 3 distinct groups of numbers
+  expect(APIKeys.isAlphaNumeric("abc 1 2 2")).toStrictEqual(false);
+});
