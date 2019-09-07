7d418163f321bd2935784354c5a3e857261f3c4f
Thomas Piccirello <thomas@doppler.com>
2019-09-07 02:24:28 -0700

    Disable additional glob lib features to attempt to speed up matching

--- src/globs.js
+++ src/globs.js
@@ -3,6 +3,12 @@ const picomatch = require('picomatch');
 const MatchOptions = {
   nocase: true,
   dot: true,
+  lookbehinds: false,
+  nobrace: true,
+  noextglob: true,
+  nonegate: true,
+  noquantifiers: true,
+  strictBrackets: true,
 };
 
 const isMatch = (text, pattern) => picomatch.isMatch(text, pattern, MatchOptions);
